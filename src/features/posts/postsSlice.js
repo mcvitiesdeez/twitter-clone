ba26f8f8ff5405871a7b6a32f9daebddda44a048
Teng Tjun Ong <ott_94@hotmail.com>
2024-10-06 01:44:53 +0800

    Firestore Commit

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,34 +1,57 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { jwtDecode } from "jwt-decode";
 import axios from "axios";
-
+import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore"
+import { db } from "../../firebase";
 const BASE_URL = "https://b304203c-0525-4a6f-b6c1-7c59d32a26cc-00-d6bhwfg9bqzn.sisko.replit.dev";
 
 export const fetchPostsByUser = createAsyncThunk(
     "posts/fetchByUser",
     async (userId) => {
-        const response = await fetch(`${BASE_URL}/posts/user/${userId}`)
-        return response.json(); //Action.payload in addCase
+        // const response = await fetch(`${BASE_URL}/posts/user/${userId}`)
+        // return response.json(); //Action.payload in addCase
+        try{
+            const postsRef = collection( db,`users/${userId}/posts`);
+            const querySnapshot = await getDocs(postsRef);
+            const docs = querySnapshot.docs.map((doc) => ({
+                id:doc.id,
+                ...doc.data(),
+            }))
+
+            return docs;
+
+        } catch(error){
+            console.error(error);
+            throw error;
+        }
     }
 )
 
 export const savePost = createAsyncThunk(
     "posts/savePost",
-    async (postContent) => {
-        const token = localStorage.getItem("authToken");
+    async ({ userId,postContent }) => {
+        // const token = localStorage.getItem("authToken");
 
-        //Decode the token to fetch user id
-        const decode = jwtDecode(token);
-        const userId = decode.id // May change depending on how the server encode the token
+        // //Decode the token to fetch user id
+        // const decode = jwtDecode(token);
+        // const userId = decode.id // May change depending on how the server encode the token
 
-        //Prepare data to be sent
-        const data = {
-        title: "Post Title",
-        content: postContent,
-        user_id: userId, 
-        };
-        const response = await axios.post(`${BASE_URL}/posts`, data)
-        return response.data
+        // //Prepare data to be sent
+        // const data = {
+        // title: "Post Title",
+        // content: postContent,
+        // user_id: userId, 
+        // };
+        // const response = await axios.post(`${BASE_URL}/posts`, data)
+        // return response.data
+
+        try{
+            const postsRef = collection(db, `users/${userId}/posts`);
+            console.log(`users/${userId}/posts`)
+        } catch(error){
+            console.error(error);
+            throw error;
+        }
     }
 )
 
